5d2cfb52218b53c923622abf9a316f06f1b30ee0
Ryan Munger <ryanmunger@gmail.com>
2017-01-09 15:14:26 -0500

    Add first view and express ejs layouts

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
+app.use(express.static(`${__dirname}/public`));
+
 app.get('/', (req, res) => {
-  res.send('Hello there');
+  res.render('pages/home', { pageTitle: 'Home' });
 });
 
 app.listen(port, () => {
