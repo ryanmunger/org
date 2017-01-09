eebcde573dcb55805271d2b949c347d660ece774
Ryan Munger <ryanmunger@gmail.com>
2017-01-09 10:51:00 -0500

    Ensure staging const is set to server env

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  res.send('Hello there');
 });
 
 app.listen(port, () => {
